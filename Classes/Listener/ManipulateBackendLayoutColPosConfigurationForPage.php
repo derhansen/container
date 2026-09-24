75428784c4e95d553f792c9be855ade47f1ba312
Torben Hansen <derhansen@gmail.com>
2026-09-24 09:27:56 +0200

    [BUGFIX] Prevent undefined array key warning in ManipulateBackendLayoutColPosConfigurationForPage

--- Classes/Listener/ManipulateBackendLayoutColPosConfigurationForPage.php
+++ Classes/Listener/ManipulateBackendLayoutColPosConfigurationForPage.php
@@ -42,8 +42,8 @@ class ManipulateBackendLayoutColPosConfigurationForPage
         $cType = $container->getCType();
         $configuration = $this->tcaRegistry->getContentDefenderConfiguration($cType, $e->colPos);
         $e->configuration = [
-            'allowedContentTypes' => $configuration['allowedContentTypes'],
-            'disallowedContentTypes' => $configuration['disallowedContentTypes'],
+            'allowedContentTypes' => $configuration['allowedContentTypes'] ?? '',
+            'disallowedContentTypes' => $configuration['disallowedContentTypes'] ?? '',
         ];
     }
 
